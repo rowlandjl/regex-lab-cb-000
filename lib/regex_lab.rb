b0f4f9e60647273b07db61c8fb06c4170a1134d4
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-05 01:08:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/^un\w\ing/)
 end
 
 def words_five_letters_long(text)
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z]\[.]^/) ? true:false 
+  text.match(/^[A-Z]\[.]^/) ? true:false
 end
 
 def valid_phone_number?(phone)
